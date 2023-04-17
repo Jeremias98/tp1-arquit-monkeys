e902f3228ad7334d67cb0d840a260356c5e45468
Jeremías <ramirezjeremias98@gmail.com>
2023-04-16 23:44:35 -0300

    add: errorHandler middleware

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,7 @@ app.get('/', (_, res) => res.send('Hello World!'));
 
 app.get('/ping', (_, res) => res.send('ping'));
 
-app.get('/metar', (req, res) => {
+app.get('/metar', (req, res, _next) => {
     const { station } = req.query;
     // TODO: make a HTTP request to NOAA's API
     res.send(station);
@@ -16,5 +17,7 @@ app.get('/metar', (req, res) => {
 app.get('/space_news', (_, res) => res.send('space_news'));
 
 app.get('/fact', (_, res) => res.send('fact'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
