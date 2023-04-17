fdc8b5dd9133af82b584a21de305d35eb05242c4
Jeremías <ramirezjeremias98@gmail.com>
2023-04-16 22:48:26 -0300

    add: empty endpoints

--- app/app.js
+++ app/app.js
@@ -3,6 +3,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (_, res) => res.send('Hello World!'));
+
+app.get('/ping', (_, res) => res.send('ping'));
+
+app.get('/metar', (req, res) => {
+    const { station } = req.query;
+    // TODO: make a HTTP request to NOAA's API
+    res.send(station);
+});
+
+app.get('/space_news', (_, res) => res.send('space_news'));
+
+app.get('/fact', (_, res) => res.send('fact'));
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
